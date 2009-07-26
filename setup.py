516c1555bf7bf0c3412e42ada5695a0c4ce2d0d1
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2009-07-26 19:26:26 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name            = 'pyinputvalidation',
+        version         = '0.1',
+        description     = 'Highly-flexible, no-magic input validation library',
+        author          = 'Felix Schwarz',
+        author_email    = 'felix.schwarz@oss.schwarz.eu',
+        url             = 'http://www.schwarz.eu/oss/projects/pyinputvalidation/',
+        packages        = ['inputvalidation'],
+        license         = 'MIT',
+    )
+
+
