558963f9c84d049808dd7e8ea1040a38c8c3682a
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2010-01-30 13:10:14 +0100

    added project meta information (license, changelog)

---
+++ pycerberus/release.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+"Release information about pycerberus."
+
+name = 'pycerberus'
+version = '0.1dev'
+description = 'Highly flexible, no magic input validation library'
+long_description = '''
+pycerberus is a framework to check user data thoroughly so that you can protect
+your application from malicious (or just garbled) input data.
+
+* Remove stupid code which converts input values: After values are validated, you 
+  can work with real Python types instead of strings - e.g. 42 instead of '42', 
+  convert database IDs to model objects transparently.
+* Implement custom validation rules: Writing custom validators is 
+  straightforward, everything is well documented and pycerberus only uses very 
+  little Python magic.
+* Focus on your value-adding application code: Save time by implementing every 
+  input validation rule only once, but 100% right instead of implementing a 
+  dozen different half-baked solutions.
+* Ready for global business: i18n support (based on GNU gettext) is built in, 
+  adding custom translations is easy.
+* Tune it for your needs: You can implement custom behavior in your validators,
+  e.g. fetch translations from a database instead of using gettext or define
+  custom translations for built-in validators.
+* Use it wherever you like: pycerberus does not depend on specific contexts
+  (e.g. web development) so you can also use it in every Python application. 
+
+
+Changelog
+******************************
+
+0.1 (30.01.2010)
+==================
+ - initial release
+'''
+author = 'Felix Schwarz'
+email = 'felix.schwarz@oss.schwarz.eu'
+url = 'http://www.schwarz.eu/opensource/projects/pycerberus'
+download_url = 'http://www.schwarz.eu/opensource/projects/%(name)s/download/%(version)s/%(name)s-%(version)s.tar.gz' % dict(name=name, version=version)
+copyright = u'2009-2010 Felix Schwarz'
+license='MIT'
+
--- setup.py
+++ setup.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import os
+
 from setuptools import setup
+
+execfile(os.path.join('pycerberus', 'release.py'))
 
 if __name__ == '__main__':
     setup(
-        name            = 'pycerberus',
-        version         = '0.1',
-        description     = 'Highly flexible, no magic input validation library',
-        author          = 'Felix Schwarz',
-        author_email    = 'felix.schwarz@oss.schwarz.eu',
-        url             = 'http://www.schwarz.eu/oss/projects/pycerberus/',
-        packages        = ('pycerberus',),
-        license         = 'MIT',
+          name=name,
+          version=version,
+
+          description=description,
+          long_description=long_description,
+          author=author,
+          author_email=email,
+          url=url,
+          download_url=download_url,
+          license=license,
         
-        install_requires = ('setuptools',)
+          zip_safe=True,
+          packages=('pycerberus',),
+          classifiers = (
+              'Development Status :: 4 - Beta',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved :: MIT License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Topic :: Software Development :: Libraries :: Python Modules',
+          ),
+          test_suite = 'nose.collector',
     )
 
 
