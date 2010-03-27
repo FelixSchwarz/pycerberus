e80c057a8ec96792ed3191e204445dfc6bfc2910
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2010-03-27 16:38:31 +0100

    IntegerValidator can check for a minimum and maximum value (closes #9)

--- pycerberus/validators/basic_numbers.py
+++ pycerberus/validators/basic_numbers.py
@@ -23,6 +23,7 @@
 # THE SOFTWARE.
 
 from pycerberus.api import Validator
+from pycerberus.errors import InvalidArgumentsError
 from pycerberus.i18n import _
 
 __all__ = ['IntegerValidator']
@@ -30,13 +31,23 @@ __all__ = ['IntegerValidator']
 
 class IntegerValidator(Validator):
     
+    def __init__(self, min=None, max=None, *args, **kwargs):
+        self.min = min
+        self.max = max
+        if (self.min is not None) and (self.max is not None) and (self.min > self.max):
+            message = 'min must be smaller or equal to max (%s > %s)' % (repr(self.min), repr(self.max))
+            raise InvalidArgumentsError(message)
+        self.super(*args, **kwargs)
+    
     def messages(self):
         return {
                 'invalid_type': _(u'Validator got unexpected input (expected string, got "%(classname)s").'),
                 'invalid_number': _(u'Please enter a number.'),
+                'too_low': _(u'Number must be %(min)d or greater.'),
+                'too_big': _(u'Number must be %(max)d or smaller.'),
                }
     
-    def convert(self, value, context=None):
+    def convert(self, value, context):
         if not isinstance(value, (int, basestring)):
             classname = value.__class__.__name__
             self.error('invalid_type', value, context, classname=classname)
@@ -44,5 +55,11 @@ class IntegerValidator(Validator):
             return int(value)
         except ValueError:
             self.error('invalid_number', value, context)
+    
+    def validate(self, value, context):
+        if (self.min is not None) and (value < self.min):
+            self.error('too_low', value, context, min=self.min)
+        if (self.max is not None) and (value > self.max):
+            self.error('too_big', value, context, max=self.max)
 
 
--- tests/basic_numbers_test.py
+++ tests/basic_numbers_test.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 from pycerberus import InvalidDataError
+from pycerberus.errors import InvalidArgumentsError
 from pycerberus.test_util import ValidationTest
 from pycerberus.validators import IntegerValidator
 
@@ -25,5 +26,35 @@ class IntegerValidatorTest(ValidationTest):
         self.assert_raises(InvalidDataError, self.process, {})
         self.assert_raises(InvalidDataError, self.process, object)
         self.assert_raises(InvalidDataError, self.process, int)
+    
+    def test_validator_rejects_none_if_value_is_required(self):
+        # Actually this functionality seems to be pretty basic and is 
+        # implemented in the Validator class - however it was broken because of
+        # a bug in simple_super ("self.super()" vs. "self.super(*args, **kwargs)")
+        self.assert_raises(InvalidDataError, self.process, None)
+    
+    def test_can_specify_minimum_value(self):
+        self.init_validator(min=20)
+        self.process(20)
+        self.process(40)
+        e = self.assert_raises(InvalidDataError, lambda: self.process(4))
+        self.assert_equals('Number must be 20 or greater.', e.msg())
+    
+    def test_can_specify_maximum_value(self):
+        self.init_validator(max=12)
+        self.process(12)
+        self.process(-5)
+        e = self.assert_raises(InvalidDataError, lambda: self.process(13))
+        self.assert_equals('Number must be 12 or smaller.', e.msg())
+    
+    def test_can_use_min_and_max_together(self):
+        self.init_validator(min=3, max=12)
+        self.process(5)
+        self.assert_raises(InvalidDataError, lambda: self.process(2))
+        self.assert_raises(InvalidDataError, lambda: self.process(13))
+    
+    def test_minium_value_must_be_smaller_or_equal_to_maximum(self):
+        e = self.assert_raises(InvalidArgumentsError, lambda: self.init_validator(min=13, max=12))
+        self.assert_equals('min must be smaller or equal to max (13 > 12)', e.msg())
 
 
