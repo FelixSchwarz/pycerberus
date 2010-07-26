7e43dd2bbc4851089ff3cbfc3652d23927fd9057
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2010-07-27 00:04:35 +0200

    move code from setup.py into its own module to make setup.py more readable

--- setup.py
+++ setup.py
@@ -5,45 +5,14 @@ import os
 
 import setuptools
 
+from pycerberus.distribution_helpers import commands_for_babel_support, information_from_file
 
-def is_babel_available():
-    try:
-        import babel
-    except ImportError:
-        return False
-    return True
-
-def commands_for_babel_support():
-    if not is_babel_available():
-        print 'not available'
-        return {}
-    from babel.messages import frontend as babel
-    
-    extra_commands = {
-        'extract_messages': babel.extract_messages,
-        'init_catalog':     babel.init_catalog,
-        'update_catalog':   babel.update_catalog,
-        'compile_catalog':  babel.compile_catalog,
-    }
-    return extra_commands
 
 if __name__ == '__main__':
-    execfile(os.path.join('pycerberus', 'release.py'))
-    
     extra_commands = commands_for_babel_support()
+    externally_defined_parameters= information_from_file(os.path.join('pycerberus', 'release.py'))
     
     setuptools.setup(
-          name=name,
-          version=version,
-
-          description=description,
-          long_description=long_description,
-          author=author,
-          author_email=email,
-          url=url,
-          download_url=download_url,
-          license=license,
-          
           extras_require = {
               'Babel': ['Babel>=0.9.5'],
           },
@@ -62,6 +31,7 @@ if __name__ == '__main__':
           ),
           test_suite = 'nose.collector',
           cmdclass=extra_commands,
+          **externally_defined_parameters
     )
 
 
