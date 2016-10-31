d4f55412ae1f644829ee84e371a138041b4cb5df
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2016-10-31 16:03:12 +0100

    IntegerValidatorTest: use methods from PythonicTestcase more extensively

--- tests/basic_numbers_test.py
+++ tests/basic_numbers_test.py
@@ -16,55 +16,63 @@ class IntegerValidatorTest(ValidationTest):
     validator_class = IntegerValidator
     
     def test_can_convert_string_to_int(self):
-        self.assert_equals(42, self.process('42'))
-        self.assert_equals(42, self.process(u'42'))
-        self.assert_equals(-5, self.process('-5'))
-    
+        assert_equals(42, self.process('42'))
+        assert_equals(42, self.process(u'42'))
+        assert_equals(-5, self.process('-5'))
+
     def test_validator_accepts_integer(self):
-        self.assert_equals(4, self.process(4))
-    
+        assert_equals(4, self.process(4))
+
     def test_fail_for_non_digit_strings(self):
-        assert_raises(InvalidDataError, lambda: self.process('invalid_number'))
-    
+        with assert_raises(InvalidDataError):
+            self.process('invalid_number')
+
     def test_validator_rejects_bad_types(self):
-        assert_raises(InvalidDataError, lambda: self.process([]))
-        assert_raises(InvalidDataError, lambda: self.process({}))
-        assert_raises(InvalidDataError, lambda: self.process(object))
-        assert_raises(InvalidDataError, lambda: self.process(int))
-    
+        with assert_raises(InvalidDataError):
+            self.process([])
+        with assert_raises(InvalidDataError):
+            self.process({})
+        with assert_raises(InvalidDataError):
+            self.process(object)
+        with assert_raises(InvalidDataError):
+            self.process(int)
+
     def test_validator_rejects_none_if_value_is_required(self):
         # Actually this functionality seems to be pretty basic and is 
         # implemented in the Validator class - however it was broken because of
         # a bug in simple_super ("self.super()" vs. "self.super(*args, **kwargs)")
-        assert_raises(InvalidDataError, lambda: self.process(None))
-    
+        with assert_raises(InvalidDataError):
+            self.process(None)
+
     def test_can_specify_minimum_value(self):
         self.init_validator(min=20)
         self.process(20)
         self.process(40)
         e = assert_raises(InvalidDataError, lambda: self.process(4))
-        self.assert_equals('Number must be 20 or greater.', e.msg())
-    
+        assert_equals('Number must be 20 or greater.', e.msg())
+
     def test_can_specify_maximum_value(self):
         self.init_validator(max=12)
         self.process(12)
         self.process(-5)
         e = assert_raises(InvalidDataError, lambda: self.process(13))
-        self.assert_equals('Number must be 12 or smaller.', e.msg())
-    
+        assert_equals('Number must be 12 or smaller.', e.msg())
+
     def test_can_use_min_and_max_together(self):
         self.init_validator(min=3, max=12)
         self.process(5)
-        assert_raises(InvalidDataError, lambda: self.process(2))
-        assert_raises(InvalidDataError, lambda: self.process(13))
-    
+        with assert_raises(InvalidDataError):
+            self.process(2)
+        with assert_raises(InvalidDataError):
+            self.process(13)
+
     def test_minium_value_must_be_smaller_or_equal_to_maximum(self):
         e = assert_raises(InvalidArgumentsError, lambda: self.init_validator(min=13, max=12))
-        self.assert_equals('min must be smaller or equal to max (13 > 12)', e.msg())
-    
+        assert_equals('min must be smaller or equal to max (13 > 12)', e.msg())
+
     def test_treats_empty_string_as_empty_value(self):
         self.init_validator(required=False)
-        assert_equals(None, self.process(''))
+        assert_none(self.process(''))
 
     def test_revert_conversion(self):
         assert_equals(1, self.revert_conversion(1))
