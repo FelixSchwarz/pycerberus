dfcf66d57b0b5062eabf853fbe5c1b76594e3d15
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2016-10-31 13:16:47 +0100

    shorten license headers

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+# This file is a part of pycerberus.
+# The source code contained in this file is licensed under the MIT license.
+# See LICENSE.txt in the main project directory, for more information.
 from __future__ import absolute_import, unicode_literals
 
 from distutils.command.build import build
