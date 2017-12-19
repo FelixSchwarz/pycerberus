332f0c665e2b99a3b4180703a36eca6e416d7366
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2017-12-19 18:03:22 +0100

    update "lib/attribute_dict.py" to version 1.1 (set items via attributes)

--- pycerberus/lib/attribute_dict.py
+++ pycerberus/lib/attribute_dict.py
@@ -3,8 +3,11 @@
 # License: Public Domain
 # Authors: Felix Schwarz <felix.schwarz@oss.schwarz.eu>
 # 
-# Version 1.0
-
+# Version 1.1
+#
+# 1.1 (08.09.2015)
+#   - set items via attributes
+#
 # 1.0 (06.02.2010)
 #   - initial release
 
@@ -20,20 +23,32 @@ class AttrDict(dict):
             raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, name))
         return self[name]
 
+    def __setattr__(self, name, value):
+        if name not in self:
+            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, name))
+        self[name] = value
+
 
 class AttributDictTests(TestCase):
-    
     def test_can_use_class_as_dict(self):
         obj = AttrDict(foo=1, bar=2)
         self.assertEquals(1, obj['foo'])
         self.assertEquals(2, obj['bar'])
-    
+
     def test_can_access_items_as_attributes(self):
         obj = AttrDict(foo=1, bar=2)
         self.assertEquals(1, obj.foo)
         self.assertEquals(2, obj.bar)
-    
+
+    def test_can_set_values_via_attributes(self):
+        obj = AttrDict(foo=1, bar=2)
+        obj.foo = 21
+        self.assertEquals(21, obj.foo)
+        obj.bar = 42
+        self.assertEquals(42, obj.bar)
+
     def test_raise_attribute_error_for_non_existent_keys(self):
         obj = AttrDict(foo=1)
         self.assertRaises(AttributeError, getattr, obj, 'invalid')
+        self.assertRaises(AttributeError, setattr, obj, 'invalid', 'something')
 
