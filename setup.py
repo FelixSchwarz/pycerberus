edaf62bbdd477b8ad7f611ff430746f02e5feffb
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2021-10-13 16:16:08 +0200

    just use README.md as "long_description"
    That way "setup.py" does not contain real code anymore.

--- setup.py
+++ setup.py
@@ -3,17 +3,11 @@
 # This file is a part of pycerberus.
 # The source code contained in this file is licensed under the MIT license.
 # See LICENSE.txt in the main project directory, for more information.
-from __future__ import absolute_import, unicode_literals
 
-import os
+from __future__ import absolute_import, print_function, unicode_literals
 
 import setuptools
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
 if __name__ == '__main__':
-    setuptools.setup(
-        long_description=(read('README.md') + read('Changelog.txt')),
-    )
+    setuptools.setup()
 
